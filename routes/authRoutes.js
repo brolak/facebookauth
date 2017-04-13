65d340ce2212359ea866095542973ee6bf019f26
Mati <matiborlak@gmail.com>
2017-04-13 17:08:39 +0300

    login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,4 +2,15 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../models/passport');
 
+router.get('/facebook', 
+	passport.authenticate('facebook', { scope: ['email'] }));
+
+router.get('/facebook/callback',
+  passport.authenticate('facebook', { session: false, failureRedirect: '/' }),
+  function(req, res) {
+  	console.log(req.user);
+    // Successful authentication, redirect home.
+    res.redirect('/authorization?token='+req.user.token+"&name="+req.user.name);
+  });
+
 module.exports = router;
